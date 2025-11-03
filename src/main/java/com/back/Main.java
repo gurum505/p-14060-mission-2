ac1ec939121a9881b11ce2670976d4373ed6eeab
seho <sayhojeong505@gmail.com>
2025-11-03 11:11:11 +0900

    first

---
+++ src/main/java/com/back/Main.java
@@ -0,0 +1,17 @@
+package com.back;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        while(True){
+            System.out.print("명령:");
+            string
+
+        }
+    }
+}
